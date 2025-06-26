8acbbacb87489a51caaf0364c21c9a56a793b9b6
M1rotv0rets <mainkraftys7654@gmail.com>
2025-06-26 18:55:21 +0700

    Update For icon (Not code).cs

--- For icon (Not code).cs
+++ For icon (Not code).cs
@@ -1,1 +1,10 @@
 !!!THE SOURCE CODE AND WORKING COMPILED VERSION ARE IN RELEASES.!!!
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Привет, мир!");
+    }
+}
